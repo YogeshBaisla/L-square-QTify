263123997e1294f0e045e286ff3027c38515e615
Yogesh Baisla <yogeshbaisla793@gmail.com>
2023-12-18 20:44:00 +0530

    Adding HeroBar

---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import Vibratingheadphone from "../../assests/vibrating-headphone.svg";
+function Search({placeholder}){
+const onSubmit= (e)=>{
+    e.preventDefault();
+}
+return (
+    <div className={styles.wrapper}>
+        <div className={styles.herotxt}>
+        <h1>
+        100 Thousand Songs, ad-free
+        Over thousands podcast episodes
+        </h1>
+        </div>
+        <div>
+        <img src={Vibratingheadphone} alt="Headphone Icon"/>
+        </div>
+    </div>
+)
+}
+export default Search;
